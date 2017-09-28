35b4eb29dbd0e7329ab1adbed765308fb96a5234
Rafael Santos <rafaels@gmail.com>
2017-09-28 00:20:01 -0400

    add last name to person class

--- Person.py
+++ Person.py
@@ -2,14 +2,16 @@ import datetime, time
 
 class Person:
     _birthDate = datetime.date.today()
-    _firstName = ""
+    _firstName = ''
+    _lastName = ''
     
-    def __init__(self, firstName, birth_year, birth_month, birth_day):
+    def __init__(self, firstName, lastName, birth_year, birth_month, birth_day):
         self._firstName = firstName
+        self._lastName = lastName
         self._birthDate = datetime.date(birth_year, birth_month, birth_day)
 
     def getAge(self):
         return datetime.date.today().year - self._birthDate.year
     
     def getName(self):
-        return (self._firstName, '')
+        return (self._firstName, self._lastName)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import Person
 
-myPerson = Person.Person("Rafael", 1976, 2, 23)
+myPerson = Person.Person('Rafael', 'Santos', 1976, 2, 23)
 
-print('my name is ' + myPerson.getName()[0] + ' and I\'m ' + str(myPerson.getAge()) + ' years old.')
+print('my name is ' + myPerson.getName()[0] + ' ' + myPerson.getName()[1] + ' and I\'m ' + str(myPerson.getAge()) + ' years old.')
