7278b59f6da716462a670f71c31e8d20ba7cc3dd
Rafael Santos <rafaels@gmail.com>
2018-10-29 14:21:40 -0400

    learning inheritance

--- Person.py
+++ Person.py
@@ -1,17 +1,17 @@
 import datetime, time
 
 class Person:
-    _birthDate = datetime.date.today()
-    _firstName = ''
-    _lastName = ''
+    _birth_date = datetime.date.today()
+    _first_name = ''
+    _last_name = ''
     
-    def __init__(self, firstName, lastName, birth_year, birth_month, birth_day):
-        self._firstName = firstName
-        self._lastName = lastName
-        self._birthDate = datetime.date(birth_year, birth_month, birth_day)
+    def __init__(self, first_name, last_name, birth_year, birth_month, birth_day):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._birth_date = datetime.date(birth_year, birth_month, birth_day)
 
     def getAge(self):
-        return datetime.date.today().year - self._birthDate.year
+        return datetime.date.today().year - self._birth_date.year
     
     def getName(self):
-        return (self._firstName, self._lastName)
+        return (self._first_name, self._last_name)
---
+++ inheritance.py
@@ -0,0 +1,11 @@
+from Person import Person
+
+class FxArtist(Person):
+    def __init__(self, first_name, last_name, birth_year, birth_month, birth_day):
+        super(Person, self).__init__(first_name, last_name, birth_year, birth_month, birth_day)
+
+
+if __name__ == '__main__':
+    my_artist = FxArtist('Rafael', 'Santos', 1976, 2, 23)
+    print('my artist name is ' + my_artist.getName()[0] + ' ' + my_artist.getName()[1] + ' and I\'m ' + str(my_artist.getAge()) + ' years old.')
+
